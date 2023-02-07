a5872d7778fce7c36cc61fd72b6adf0c76f1d1fd
s-caluk <seher.caluk@gmail.com>
2023-02-07 15:57:52 +0100

    V04

--- src/test/java/video04_webElements/WebElements02.java
+++ src/test/java/video04_webElements/WebElements02.java
@@ -19,18 +19,27 @@ public class WebElements02 {
 
         // https://www.facebook.com adresine gidin
         driver.get("https:www.facebook.com");
+
         // cookies cikarsa kabul et butonuna basin
-        driver.findElement(By.xpath("//button[@title='Allow essential and optional cookies']")).click();
+        //driver.findElement(By.xpath("//button[@title='Allow essential and optional cookies']")).click();
+
+        // cookies cikarsa kabul et butonuna basin - almanca facebook´a güncelledim. Otomatik click yapacak.
+        driver.findElement(By.xpath("//button[@title='Erforderliche und optionale Cookies erlauben']")).click();
+
         // e-posta kutusuna rastgele bir mail girin
         WebElement epostaKutusu= driver.findElement(By.xpath("//input[@id='email']"));
         epostaKutusu.sendKeys("Hlkiujhgtrf");
+
         // sifre kismina rastgele bir sifre girin
         WebElement passKutusu= driver.findElement(By.xpath("//input[@id='pass']"));
         passKutusu.sendKeys("ydgfkshd");
+
         // giris yap butonuna basin
         driver.findElement(By.xpath("//button[@name='login']")).click();
-        // uyari olarak "The email or mobile number you entered isn’t connected to an account. Find your account and log in."
-        //  mesajinin ciktigini test edin
+
+        /*
+        // uyari olarak "The email or mobile number you entered isn’t connected to an account.
+        // Find your account and log in."  mesajinin ciktigini test edin
         WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='_9ay7']"));
         String expectedSonucYazisi="The email or mobile number you entered isn’t connected to an account. Find your account and log in.";
         String actualSonucYazisi=sonucYaziElementi.getText();
@@ -39,6 +48,20 @@ public class WebElements02 {
         } else {
             System.out.println("girilemedi testi failed");
         }
+        */
+
+        //uyari olarak "Die eingegebenen E-Mail-Adresse ist mit keinem Konto verknüpft.
+        // Konto finden und anmelden."  mesajinin ciktigini test edin.
+        //expected olmasini istedigin sonuc! actuel ise olan sonuc! birbiri ile örtüsüyorsa ve örtüsmüyorsa...sout
+        WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='_9ay7']"));
+        String expectedSonucYazisi="Die eingegebene E-Mail-Adresse oder Handynummer ist mit keinem Konto verknüpft. Konto finden und anmelden.";
+        String actualSonucYazisi=sonucYaziElementi.getText();
+        if (expectedSonucYazisi.equals(actualSonucYazisi)){
+            System.out.println("girilemedi testi passed");
+        } else {
+            System.out.println("girilemedi testi failed");
+        }
+
         // sayfayi kapatin
         driver.close();
     }
--- src/test/java/video04_webElements/WebElements03.java
+++ src/test/java/video04_webElements/WebElements03.java
@@ -16,17 +16,17 @@ public class WebElements03 {
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         // 1- https://www.bestbuy.com adresine gidin
         driver.get("https://www.bestbuy.com");
-        // 2- cookies cikarsa kabul et butonuna basin
+
+        // 2- cookies cikarsa kabul et butonuna basin (burda cookies cikmadi)
 
         // 3- sayfada kac adet button bulundugunu yazdirin
-
         List<WebElement> buttonListesi=driver.findElements(By.tagName("button"));
         System.out.println(buttonListesi.size());
 
         // 4- sayfadaki herbir button uzerindeki yazilari yazdirin
-
         for (WebElement each:buttonListesi
              ) {
             System.out.println(each.getText());
