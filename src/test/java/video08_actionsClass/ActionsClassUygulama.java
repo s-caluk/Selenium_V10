838467739ccd3c7751cc7fd5879b5c4049fd048b
s-caluk <seher.caluk@gmail.com>
2023-02-08 15:04:52 +0100

    V07

--- src/test/java/video08_actionsClass/ActionsClassUygulama.java
+++ src/test/java/video08_actionsClass/ActionsClassUygulama.java
@@ -20,12 +20,15 @@ public class ActionsClassUygulama {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         //2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
+
         //3. Click on 'Signup / Login' button
         WebElement signupElementi=driver.findElement(By.xpath("//a[text()=' Signup / Login']"));
         Actions actions=new Actions(driver);
         actions.click(signupElementi).perform();
+
         //4. Enter name and email address
         //5. Click 'Signup' button
         WebElement nameKutusu= driver.findElement(By.xpath("//input[@data-qa='signup-name']"));
@@ -36,10 +39,13 @@ public class ActionsClassUygulama {
                 .sendKeys(Keys.TAB)
                 .sendKeys(Keys.ENTER)
                 .perform();
+
         //6. Fill details: Title, Name, Email, Password, Date of birth
         //7. Select checkbox 'Sign up for our newsletter!'
         //8. Select checkbox 'Receive special offers from our partners!'
+
         //9. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+             //17 tane locat yapmaktan kurtardi actions Class!
         WebElement titleRadiButton= driver.findElement(By.xpath("//div[@id='uniform-id_gender1']"));
         actions.click(titleRadiButton       )
                 .sendKeys(Keys.TAB)
